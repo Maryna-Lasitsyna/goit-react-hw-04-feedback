13071a058bb5c206ccbac3c0703e5b837ff8168d
Maryna-Lasitsyna <121902242+Maryna-Lasitsyna@users.noreply.github.com>
2023-08-03 15:15:54 +0300

    feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,76 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { useState } from 'react';
+import FeedbackOptions from './FeedbackOptions';
+import Section from './Section';
+import Statistics from './Statistics';
+import Notification from './Notification';
+import { Container} from './App.styled';
+
+
+const App = () => {
+
+  const [state, setState] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+
+  const handleButtonClick = (type) => {
+   setState((prevState) => ({
+      ...prevState,
+      [type]: prevState[type] + 1
+    }));
+  };
+
+  const countTotalFeedback = () => {
+    const { good, neutral, bad } = state;
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const { good } = state;
+    const total = countTotalFeedback();
+    return total === 0 ? 0 : Math.round((good / total) * 100);
+  };
+
+
+  const { good, neutral, bad } = state;
+  const totalFeedback = countTotalFeedback();
+  const positiveFeedbackPercentage = countPositiveFeedbackPercentage();
+  let feedbackContent;
+
+  if (totalFeedback === 0) {
+    feedbackContent = <Notification message="There is no feedback" />;
+  } else {
+    feedbackContent = (
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={totalFeedback}
+        positivePercentage={positiveFeedbackPercentage}
+      />
+    );
+  }
+   
+
+    return (
+      <Container>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={handleButtonClick}
+          />
+        </Section>
+        <Section title='Statistics'>
+          {feedbackContent}
+        </Section>
+      </Container>
+    );
+  }
+
+
+export default App;
+
+
+
---
+++ src/components/Statistics/Statistics.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 24px;
+`;
+
+export const Item = styled.p`
+  font-size: 16px;
+  margin-bottom: 5px;
+`;
+
+
